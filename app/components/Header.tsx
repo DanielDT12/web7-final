ace3b0b9b4acc8837d053e1e944e94c0d10e2f75
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-16 14:24:00 +0200

    propping av form states

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,10 @@
 import { SearchForm } from "./SearchForm";
 
-export const Header = () => {
+export const Header = ({ form, setForm }: any) => {
 	return (
 		<header className="flex justify-between p-8">
 			<h1 className="text-2xl">Web 7 Final</h1>
-			<SearchForm />
+			<SearchForm form={form} setForm={setForm} />
 		</header>
 	);
 };
