4c6b8c9855273216e3ac6a262685db6a127f1a38
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-16 14:23:20 +0200

    hevet form state til page, for og sende form verdi til data fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import DataFetching from "./components/getDataList";
 import { Header } from "./components/Header";
+import { useState } from "react";
 
 export default function Home() {
+	const [form, setForm] = useState({
+		kommune: "",
+		year: "",
+	});
 	return (
 		<main className="flex flex-col px-4 font-[family-name:var(--font-geist-sans)]">
-			<Header />
-			<DataFetching />
+			<Header form={form} setForm={setForm} />
+			<DataFetching form={form} setForm={setForm} />
 		</main>
 	);
 }
