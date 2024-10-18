433c411eef28a1aa79b37b4ec9852d566f2f21fc
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-18 14:04:38 +0200

    add basic styling table

--- app/components/DataTable.tsx
+++ app/components/DataTable.tsx
@@ -9,17 +9,23 @@ export default function DataTable({ data }: DataTableProps) {
 		<table className="border border-solid border-red-600 w-[100%]">
 			<thead>
 				<tr>
-					<th>Navn</th>
-					<th>Org.Nr</th>
-					<th>Stiftelsesdato</th>
+					<th className="border-t border border-white p-2 text-left">Navn</th>
+					<th className="border-t border border-white p-2 text-left">Org.Nr</th>
+					<th className="border-t border border-white p-2 text-left">
+						Stiftelsesdato
+					</th>
 				</tr>
 			</thead>
 			<tbody>
 				{data._embedded?.enheter?.map((bedrift) => (
 					<tr key={bedrift.organisasjonsnummer}>
-						<td>{bedrift.navn}</td>
-						<td>{bedrift.organisasjonsnummer}</td>
-						<td>{bedrift.stiftelsesdato}</td>
+						<td className="p-2 border-t border border-white">{bedrift.navn}</td>
+						<td className="p-2 border-t border border-white">
+							{bedrift.organisasjonsnummer}
+						</td>
+						<td className="p-2 border-t border border-white">
+							{bedrift.stiftelsesdato}
+						</td>
 					</tr>
 				))}
 			</tbody>
