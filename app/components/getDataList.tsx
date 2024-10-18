f32b5d7d4e70858ac1292e11a5ec9aa520d77804
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-18 11:32:08 +0200

    add comments, formated URL fetch, form data check

--- app/components/getDataList.tsx
+++ app/components/getDataList.tsx
@@ -9,28 +9,34 @@ const KOMMUNE = "kommunenummer=";
 const FRA_DATO = "fraStiftelsesdato=";
 const TIL_DATO = "tilStiftelsesdato=";
 
-export default function DataFetching({ form, setForm, pageNumber }: any) {
+export default function DataFetching({ form, setForm, pageNumber = 0 }: any) {
 	const [bedrifter, setBedrifter] = useState({});
 
 	const [isLoading, setIsloading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const abortControllerRef = useRef<AbortController | null>(null);
+	const abortControllerRef = useRef<AbortController | null>(null); // abortcontroller referanse for og håndtere race conditions
 
 	const kommuneNavnNormalisert =
-		form.kommune.charAt(0).toUpperCase() + form.kommune.slice(1).toLowerCase();
-	const kommuneNummer = kommuneNavnOgNummer[kommuneNavnNormalisert] || "ukjent";
+		form?.kommune.charAt(0).toUpperCase() +
+		form?.kommune.slice(1).toLowerCase(); // normalisering av string input fra form
+
+	const kommuneNummer = kommuneNavnOgNummer[kommuneNavnNormalisert] || "ukjent"; // setter string til kommune nr for.eks bergen til 4601
 
 	useEffect(() => {
+		if (!form || !form.kommune || !form.year) return;
+
 		const fetchBedrifter = async () => {
-			abortControllerRef.current?.abort();
-			abortControllerRef.current = new AbortController();
+			const FORMATED_URL = `${BASE_URL}?${KOMMUNE}${kommuneNummer}&${FRA_DATO}${form.year}-01-01&${TIL_DATO}${form.year}-12-31&page=${pageNumber}`;
+
+			abortControllerRef.current?.abort(); // sjekker om det er en activ abortcontroller, hvis det finnes så kjører den abort
+			abortControllerRef.current = new AbortController(); // lager ny abortcontroller
 
 			setIsloading(true);
 			setError(null);
 
 			try {
-				const res = await fetch(BASE_URL, {
+				const res = await fetch(FORMATED_URL, {
 					signal: abortControllerRef.current?.signal,
 				});
 
@@ -51,10 +57,9 @@ export default function DataFetching({ form, setForm, pageNumber }: any) {
 		};
 
 		fetchBedrifter();
-	}, []);
+	}, [form]);
 
-	const FORMATED_URL = `${BASE_URL}?${KOMMUNE}${kommuneNummer}&${FRA_DATO}${form.year}-01-01&${TIL_DATO}${form.year}-12-31&page=0`;
-	console.log(FORMATED_URL);
+	console.log(bedrifter);
 
 	if (isLoading) {
 		return <div>Loading...</div>;
@@ -66,7 +71,11 @@ export default function DataFetching({ form, setForm, pageNumber }: any) {
 
 	return (
 		<div className="grid grid-cols-4 gap-8 px-8">
-			<p></p>
+			{bedrifter && Object.keys(bedrifter).length > 0 ? (
+				<p>Data Fetching OK</p>
+			) : (
+				<p>No data</p>
+			)}
 		</div>
 	);
 }
