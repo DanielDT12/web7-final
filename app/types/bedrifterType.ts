bdd4927089eb08384e1974addfa9123b1efb1448
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-21 15:13:42 +0200

    add new typing for page numbers

--- app/types/bedrifterType.ts
+++ app/types/bedrifterType.ts
@@ -5,8 +5,16 @@ interface BedriftData {
 	konkurs: boolean;
 }
 
+interface PageNumberInfo {
+	sizeOfElementsIncluded: number;
+	totalElements: number;
+	totalPages: number;
+	pageNumber: number;
+}
+
 export interface BedriftResponse {
 	_embedded: {
 		enheter: BedriftData[];
 	};
+	page: PageNumberInfo;
 }
