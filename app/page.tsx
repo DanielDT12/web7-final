96fb395f5a9c392fc3cfe6554fca38858987d241
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-21 15:12:40 +0200

    add context provider for components

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,15 @@
 
 import DataFetching from "./components/getDataList";
 import { Header } from "./components/Header";
-import { useState } from "react";
+import { BedriftDataApiProvider } from "./context/bedriftApiContext";
 
 export default function Home() {
-	const [form, setForm] = useState({
-		kommune: "",
-		year: "",
-	});
 	return (
 		<div className="flex flex-col px-4 font-[family-name:var(--font-geist-sans)]">
-			<Header form={form} setForm={setForm} />
-			<DataFetching form={form} setForm={setForm} />
+			<BedriftDataApiProvider>
+				<Header />
+				<DataFetching />
+			</BedriftDataApiProvider>
 		</div>
 	);
 }
