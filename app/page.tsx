deaff6c5257f9b29ab18d2fc4e96dea20b90cb39
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-22 12:24:13 +0200

    add form state context provider

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@
 import DataFetching from "./components/getDataList";
 import { Header } from "./components/Header";
 import { BedriftDataApiProvider } from "./context/bedriftApiContext";
+import { FormStateContextProvider } from "./context/formStateContext";
 
 export default function Home() {
 	return (
 		<BedriftDataApiProvider>
-			<div className="flex flex-col px-4 font-[family-name:var(--font-geist-sans)]">
-				<Header />
-				<DataFetching />
-			</div>
+			<FormStateContextProvider>
+				<div className="flex flex-col px-4 font-[family-name:var(--font-geist-sans)]">
+					<Header />
+					<DataFetching />
+				</div>
+			</FormStateContextProvider>
 		</BedriftDataApiProvider>
 	);
 }
