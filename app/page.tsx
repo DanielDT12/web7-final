ded3bf8e43b095f9d4d8e5eabc5b88e07013e2b6
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-22 19:39:36 +0200

    moved main container from DataFetching out to page component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
 			<FormStateContextProvider>
 				<div className="flex flex-col px-4 font-[family-name:var(--font-geist-sans)]">
 					<Header />
-					<DataFetching />
+					<main className="flex flex-col justify-center max-w-[65rem] w-full mx-auto">
+						<DataFetching />
+					</main>
 				</div>
 			</FormStateContextProvider>
 		</BedriftDataApiProvider>
