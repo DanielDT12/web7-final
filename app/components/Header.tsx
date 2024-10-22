c144d0e75455e77550a39523458588afab30fa76
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-22 19:41:30 +0200

    add styling header container

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,8 @@ import { SearchForm } from "./SearchForm";
 
 export const Header = () => {
 	return (
-		<header className="flex justify-between p-8">
-			<h1 className="text-2xl">Web 7 Final</h1>
+		<header className="flex flex-col justify-between items-center gap-4 p-8 max-w-[65rem] m-auto">
+			<h1 className="text-2xl leading-none">Web 7 Final</h1>
 			<SearchForm />
 		</header>
 	);
