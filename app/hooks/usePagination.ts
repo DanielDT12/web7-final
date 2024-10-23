dfc1202c8d50a1ffec4f54d638fdf69bd4ad0528
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-23 22:42:03 +0200

    build hook for pagination

---
+++ app/hooks/usePagination.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+type PaginationPros = {
+	totalPages: number;
+	initialPage?: number;
+};
+
+export const usePagination = ({
+	totalPages,
+	initialPage = 0,
+}: PaginationPros) => {
+	const [currentPage, setCurrentPage] = useState<number>(0);
+
+	const goToPreviousPage = () => {
+		setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+	};
+
+	const goToNextPage = () => {
+		setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+	};
+
+	return { currentPage, goToNextPage, goToPreviousPage };
+};
