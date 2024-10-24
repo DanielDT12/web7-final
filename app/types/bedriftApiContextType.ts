04ae13dfd276cd1d3be728d3de771a8fc895b29c
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-24 14:13:15 +0200

    rm unused props from type

--- app/types/bedriftApiContextType.ts
+++ app/types/bedriftApiContextType.ts
@@ -3,6 +3,4 @@ import { BedriftResponse } from "./bedrifterType";
 export type BedriftApiContextType = {
 	bedrifter: BedriftResponse | null;
 	setBedrifter: React.Dispatch<React.SetStateAction<BedriftResponse | null>>;
-	currentPage: number;
-	setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 };
